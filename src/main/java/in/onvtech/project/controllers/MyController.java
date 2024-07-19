dbcc6e98569dd3966abb72c94f53f13416c12b63
TrickyVikram <vkumarsah999@gmail.com>
2024-07-20 04:05:39 +0530

    “change”

--- src/main/java/in/onvtech/project/controllers/MyController.java
+++ src/main/java/in/onvtech/project/controllers/MyController.java
@@ -3,18 +3,42 @@ package in.onvtech.project.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @Controller
 public class MyController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     @ResponseBody
-    public String home(){
+    public String home() {
 
-        return "home";
-        
+        return "body";
+
     }
 
-    
+    @GetMapping("user_list")
+    public String userList() {
+        return "user";
+    }
+
+    @PostMapping("/user_add")
+    public String userAdd() {
+
+        return "user";
+    }
+
+    @PutMapping("user_update/{id}")
+    public String userUpdate() {
+
+        return "user";
+    }
+
+    @GetMapping("user_delete/{id}")
+    public String userDelete() {
+
+        return "user";
+    }
+
 }
